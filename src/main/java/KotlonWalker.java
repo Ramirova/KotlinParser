4d280e395ffb36e4db282b29bfff5078ba680b7d
Unknown <rozaliya.amirova@gmail.com>
2018-10-05 17:39:00 +0300

    First version

---
+++ src/main/java/KotlonWalker.java
@@ -0,0 +1,16 @@
+import antlr4.KotlinParser;
+import antlr4.KotlinParserBaseListener;
+
+/**
+ * Created by rozaliaamirova on 05/10/2018.
+ */
+
+//public class KotlinWalker extends KotlinParserBaseListener {
+//    public void enterR(KotlinParser.Rcontext ctx ) {
+//        System.out.println( "Entering R : " + ctx.ID().getText() );
+//    }
+//
+//    public void exitR(KotlinParser.RContext ctx ) {
+//        System.out.println( "Exiting R" );
+//    }
+//}
